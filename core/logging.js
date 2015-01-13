d6bca871e5d49df67474b20abd02a577c57625e4
Bo Borgerson <bo@redfin.com>
2015-01-13 10:42:34 -0800

    Commentary in base logging module

--- core/logging.js
+++ core/logging.js
@@ -1,3 +1,15 @@
+/*
+ * Include a logger in your module with:
+ *
+ *   var logger = require('triton/core/logging').getLogger(__LOGGING_NAME__);
+ *
+ * This `logger` has methods for each level specified in logging/common.js.
+ *
+ * Example:
+ *
+ *   logger.debug("result: %s", result);
+ *
+ */
 if (SERVER_SIDE)
 	module.exports = require('./logging/server.js');
 else
