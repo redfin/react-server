f671d4eb72d5d531c0632dfab98d759222efa8fb
Bo Borgerson <bo@redfin.com>
2015-01-13 15:28:48 -0800

    Commentary explaining `time` method

--- core/logging.js
+++ core/logging.js
@@ -9,6 +9,14 @@
  *
  *   logger.debug("result: %s", result);
  *
+ * It *also* has a `time` method for timing named metrics.  Metric names
+ * should be dot-separated and be few in number (i.e. don't include object
+ * IDs or other variables with many potential values).
+ *
+ * Example:
+ *
+ *   logger.time(`result.${http_status_code}`, time_in_ms);
+ *
  */
 if (SERVER_SIDE)
 	module.exports = require('./logging/server.js');
