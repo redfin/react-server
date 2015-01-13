86ca84c1fc288bf4cdf4080f7b50945927685a88
Bo Borgerson <bo@redfin.com>
2015-01-12 16:36:24 -0800

    Auto-generate logging prefix

---
+++ buildutils/logging-name-loader.js
@@ -0,0 +1,20 @@
+var REPLACE_TOKEN = '__LOGGING_NAME__'
+,   THIS_MODULE   = /(?:[^\/]+\/node_modules\/)?triton\/buildutils\/logging-name-loader\.js$/
+,   BASE_PATH     = module.filename.replace(THIS_MODULE,'')
+
+module.exports = function(source){
+	return source.replace(REPLACE_TOKEN, loggingName.bind(this));
+}
+
+var loggingName = function(){
+	var fn = this.resourcePath;
+
+	if (fn.indexOf(BASE_PATH) != 0)
+		throw("Unable to handle "+REPLACE_TOKEN+" for "+fn);
+
+	return JSON.stringify(
+		fn.substring(BASE_PATH.length, fn.length)
+			.replace(/\.jsx?$/, '')
+			.replace(/\//g,'.')
+	);
+}
