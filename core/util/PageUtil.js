b1c2b7f78f7410ba531b4485b94e56286c8d24ae
Sasha Aickin <sasha.aickin@redfin.com>
2015-01-14 15:18:51 -0800

    Triton API Refactor: moved the promise utility function from PageUtil to the new PromiseUtil file

--- core/util/PageUtil.js
+++ core/util/PageUtil.js
@@ -1,4 +1,5 @@
-var Q = require("q");
+var Q = require("q"), 
+	PromiseUtil = require("./PromiseUtil");
 
 var PageUtil = module.exports = {
 	/**
@@ -16,38 +17,9 @@ var PageUtil = module.exports = {
 
 		// next, ensure that all elements are EarlyPromises.
 		result = result.map((element) => {
-			return PageUtil.early(Q(element));
+			return PromiseUtil.early(Q(element));
 		});
 
 		return result;
-	},
-
-	/** 
-	 * Translates a normal Promise to a simple EarlyPromise. 
-	 *     promise (required): the promise to translate.
-	 *     pendingValue (optional): the value to be returned by get while the promise is pending.
-	 *         defaults to null if not present.
-	 */
-	early(promise, pendingValue) {
-		// if it's already an EarlyPromise, just return it.
-		if (promise.get) {
-			return promise;
-		}
-
-		var value = (pendingValue !== undefined) ? pendingValue : null, 
-			error = null;
-
-		promise.then((resolvedValue) => {
-			value = resolvedValue;
-			return value;
-		}).catch((resolvedError) => {
-			error = resolvedError;
-		})
-
-		promise.get = function() { 
-			if (null !== error) throw error;
-			return value;
-		};
-		return promise;
 	}
 }
