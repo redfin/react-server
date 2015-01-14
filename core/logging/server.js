082bcb1fb814c64aa526853b26bdc6296122e7cc
Bo Borgerson <bo@redfin.com>
2015-01-14 13:19:41 -0800

    Colorize module names in server output

--- core/logging/server.js
+++ core/logging/server.js
@@ -8,15 +8,16 @@ for (var group in common.config)
 	loggers[group] = new winston.Container({});
 
 var getLoggerForConfig = function(name, group){
-	var config = common.config[group];
+	var config   = common.config[group]
+	,   colorize = getColorize()
 
 	// The `loggers` collection's `get` method auto-adds on miss, and
 	// returns existing on hit.
 	var logger = loggers[group].get(name, {
 		console: {
-			label     : name,
+			label     : colorize(name),
 			level     : config.baseLevel,
-			colorize  : true,   // TODO: Only if isatty.
+			colorize  : process.stdout.isTTY,
 			timestamp : false,  // TODO: Want this in production.
 		}
 		// TODO: Email transport for high-level logs in production.
@@ -28,6 +29,36 @@ var getLoggerForConfig = function(name, group){
 
 	return logger;
 }
+
+var getColorize = (function(){
+
+	// Get a set of visually distinct colors.
+	//
+	// Sure would be nice, here, to just say...
+	//
+	//   var colors = _.range(19, 232, 11);
+	//
+	var colors = [];
+	for (var i = 19; i < 232; i+=11)
+		colors.push(i);
+
+	// This function returns a function that colorizes text for the
+	// terminal.  Each time this function is called it returns a function
+	// that produces a new color (from our `colors` list).
+	return function(){
+
+		// Only colorize if we're attached to a terminal.
+		if (!process.stdout.isTTY)
+			return text => text;
+
+		var color = colors.shift();
+
+		// In case we wind up with more than `colors.length` loggers.
+		colors.push(color);
+
+		return text => [`\x1B[38;5;${color}m`, text, '\x1B[0m'].join('')
+	}
+})();
 
 var getLogger = stats.makeGetLogger(getLoggerForConfig);
 
