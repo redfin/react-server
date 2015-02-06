e839109e2bbe018ac5aa80e0c322663f31be6272
Sasha Aickin <sasha.aickin@redfin.com>
2015-02-06 14:43:12 -0800

    Initial proposal for triton testing framework.

--- core/spec/HelloWorldSpec.js
+++ core/spec/HelloWorldSpec.js
@@ -1,13 +1,61 @@
-var React = require("react");
+var React = require("react"),
+	helper = require("./tritonHelper");
+
+
+var port = process.env.PORT || 8769;
 
 class HelloWorldPage {
 	getElements() {
-		return <div>Hello, world!</div>;
+		return <div id="foo">Hello, world!</div>;
 	}
 }
 
-describe("Hello world page", function() {
-	if("says 'Hello, world!'", function() {
-		
+class GoodbyeWorldPage {
+	getElements() {
+		return <div id="foo">Goodbye, world!</div>;
+	}
+}
+
+class MultiElementPage {
+	getElements() { 
+		return [
+			<div id="foo1">Div1</div>, 
+			<div id="foo2">Div2</div>
+		];
+	}
+}
+
+
+describe("A basic page", function() {
+
+	helper.startTritonBeforeEach({
+		"/hello": HelloWorldPage, 
+		"/goodbye": GoodbyeWorldPage,
+		"/multi": MultiElementPage
+	}, port);
+
+	helper.teardownTritonAfterEach();
+
+	it("can say 'Hello, world!'", function(done) {
+
+		helper.getDocumentFor("/hello", port, (document) => {
+			expect(document.querySelector("div#foo").innerHTML).toMatch("Hello, world!");
+			done();
+		});
+	});
+
+	it("can say 'Goodbye, world!'", function(done) {
+		helper.getDocumentFor("/goodbye", port, (document) => {
+			expect(document.querySelector("div#foo").innerHTML).toMatch(/Goodbye/);
+			done();
+		});
+	});
+
+	it("can have multiple elements", function(done) {
+		helper.getDocumentFor("/multi", port, (document) => {
+			expect(document.querySelector("div#foo1").innerHTML).toMatch("Div1");
+			expect(document.querySelector("div#foo2").innerHTML).toMatch("Div2");
+			done();
+		});
 	});
 });
---
+++ core/spec/tritonHelper.js
@@ -0,0 +1,74 @@
+var renderMiddleware = require("../renderMiddleware"),
+	express = require("express"),
+	expressState = require('express-state'),
+	http = require("http"),
+	Browser = require('zombie');
+
+var servers = []; 
+
+// starts a simple, one page triton server.
+// routes is of the form {url: page}
+var startTritonServer = (routes, port, cb) => {
+	// first let's make a simple routes file with just one page
+	var routesForTriton = {routes:{}};
+	Object.keys(routes).forEach((url, index) => {
+		routesForTriton.routes["route" + index] = {
+			path: [url],
+			method: 'get',
+			page: () => {
+				return {
+					done: (cb) => {cb(routes[url]);}
+				}
+			}
+		};
+	});
+
+	var middleware = renderMiddleware(routesForTriton);
+	var server = express();
+	process.env.R3S_CONFIGS = process.cwd() + "/target/config/dev"
+
+	expressState.extend(server);
+	// sets the namespace that data will be exposed into client-side
+	// TODO: express-state doesn't do much for us until we're using a templating library
+	server.set('state namespace', 'RF');
+
+	server.use(middleware);
+	var httpServer = http.createServer(server);
+	httpServer.listen(port, () => cb(httpServer));
+};
+
+var stopTritonServer = (server, done) => {
+	server.close(done);
+};
+
+var getDocumentFor = (url, port, cb) => {
+	var browser = new Browser({runScripts:false});
+
+	browser.visit(`http://localhost:${port}${url}`).then(() => cb(browser.window.document));
+};
+
+var startTritonBeforeEach = (routes, port) => {
+	beforeEach((done) => {
+		startTritonServer(routes, port, s => {
+			servers.push(s); 
+			done();
+		});
+	});
+}
+
+var teardownTritonAfterEach = () => {
+	afterEach((done) => {
+		stopTritonServer(servers.pop(), done);
+	});
+}
+
+
+
+
+module.exports = {
+	startTritonServer, 
+	stopTritonServer, 
+	getDocumentFor,
+	startTritonBeforeEach,
+	teardownTritonAfterEach
+};
