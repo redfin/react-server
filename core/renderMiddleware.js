279b9dcf562a49e0bf424218007873e8b3d4c16f
Bo Borgerson <bo@redfin.com>
2015-02-13 14:02:08 -0800

    Increase bytesWritten `hi` threshold to 256k

--- core/renderMiddleware.js
+++ core/renderMiddleware.js
@@ -465,7 +465,7 @@ function setupLateArrivals(req, res, context, start, page) {
 		res.end("</body></html>");
 		logger.gauge(`countTotalRequests.${routeName}`, allRequests.length);
 		logger.gauge(`countLateArrivals.${routeName}`, notLoaded.length, {hi: 1});
-		logger.gauge(`bytesWritten.${routeName}`,req.socket.bytesWritten, {hi: 1<<16});
+		logger.gauge(`bytesWritten.${routeName}`,req.socket.bytesWritten, {hi: 1<<18});
 		logger.gauge(`concurentRequests`, ACTIVE_REQUESTS--);
 		logger.time(`allDone.${routeName}`, new Date - start);
 		page.handleComplete();
