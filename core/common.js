3c82abe7cbd44cd66e6b624dd2f6ba27ddb12861
Sasha Aickin <sasha.aickin@redfin.com>
2015-02-25 11:04:10 -0800

    Oops. Deleted a file in my last commit but didn't delete the one reference to it.

--- core/common.js
+++ core/common.js
@@ -3,7 +3,6 @@
 module.exports = {
 	BaseStore: require('./stores/BaseStore'),
 	Link: require('./components/Link'),
-	actions: require('./actions'),
 	ObjectGraph: require('./util/ObjectGraph'),
 	RequestLocalStorage: require('./util/RequestLocalStorage'),
 	getCurrentRequestContext: require('./context/RequestContext').getCurrentRequestContext,
