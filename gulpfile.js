3bafe8ba008e4f18872d1963ad780a8ebd2a6a2b
Lida Wang <lida.wang@redfin.com>
2015-07-23 15:56:43 -0700

    Exclude the test folder

--- gulpfile.js
+++ gulpfile.js
@@ -86,13 +86,12 @@ gulp.task("test", ["compileServer", "compileClient"], function() {
 });
 
 gulp.task("eslint", [], function() {
-	return gulp.src(src)
-		.pipe(codeFilter)
+	var srcMinusTest = src;
+	srcMinusTest.push("!core/test/**/*");
+	return gulp.src(srcMinusTest)
         // eslint() attaches the lint output to the eslint property
         // of the file object so it can be used by other modules.
-        .pipe(eslint({
-        	reset: true
-        }))
+        .pipe(eslint())
         // eslint.format() outputs the lint results to the console.
         // Alternatively use eslint.formatEach() (see Docs).
         .pipe(eslint.format());
