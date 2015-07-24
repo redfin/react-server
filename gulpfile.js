c3ad7409b68da3eca4ae2cff2775346269d644ba
Lida Wang <lida.wang@redfin.com>
2015-07-24 11:43:00 -0700

    Fail build with a list of all the lint errors instead of just the first one

--- gulpfile.js
+++ gulpfile.js
@@ -97,7 +97,7 @@ gulp.task("eslint", [], function() {
         .pipe(eslint.format())
         // To have the process exit with an error code (1) on
         // lint error, return the stream and pipe to failOnError last.
-        .pipe(eslint.failOnError());
+        .pipe(eslint.failAfterError());
 });
 
 // todo: where should tests go?
