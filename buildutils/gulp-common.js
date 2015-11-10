855422ec07759d1ca115660d14c7d4b5c0d97e24
Robert Gay <robert.gay@redfin.com>
2015-11-10 13:29:51 -0800

    Enabling ES6 syntax in corvair build-related files

--- buildutils/gulp-common.js
+++ buildutils/gulp-common.js
@@ -1,33 +1,8 @@
 var babel = require("gulp-babel");
 
-var ecmaScriptTransforms = [
-	"es6.arrowFunctions",
-	"es6.blockScoping",
-	"es6.classes",
-	"es6.constants",
-	"es6.destructuring",
-	// "es6.forOf",
-	"es6.modules",
-	"es6.parameters",
-	// "es6.properties.computed",
-	"es6.properties.shorthand",
-	"es6.spread",
-	"es6.templateLiterals",
-	// "es6.unicodeRegex", 
-	"es7.objectRestSpread",
-	// "es7.abstractReferences",
-	// "es7.comprehensions",
-	// "es7.exponentiationOperator",
-	"react",
-];
-
 module.exports = {
 	es6Transform: function() {
 		// stage:1 needed since es7.objectRestSpread is a stage 1 ES7 proposal
-		return babel({
-			stage:1,
-			whitelist:ecmaScriptTransforms,
-			modules: "common"
-		});
-	}
+		return babel(require("./babel-opts"));
+	},
 }
