a651de179008adfc8e713a8729fb2ca1eacd6a24
Sasha Aickin <xander76@yahoo.com>
2016-02-14 15:34:42 -0800

    Preliminary checkin of https support with self-signed certs. Hot loading webpack dev server still uses its own cert, and no ability to send in certs yet.

--- packages/react-server-cli/src/startServer.js
+++ packages/react-server-cli/src/startServer.js
@@ -1,7 +1,9 @@
 import triton from "react-server"
 import http from "http"
+import https from "https"
 import express from "express"
 import path from "path"
+import pem from "pem"
 import compression from "compression"
 import WebpackDevServer from "webpack-dev-server"
 import compileClient from "./compileClient"
@@ -17,11 +19,12 @@ export default function(routesRelativePath, {
 		minify = false,
 		compileOnly = false,
 		jsUrl,
+		httpsOptions = true,
 	} = {}) {
 
 	const routesPath = path.join(process.cwd(), routesRelativePath);
 	const routes = require(routesPath);
-	const outputUrl = jsUrl || `http://localhost:${jsPort}/`;
+	const outputUrl = jsUrl || `${httpsOptions ? "https" : "http"}://localhost:${jsPort}/`;
 
 	const {serverRoutes, compiler} = compileClient(routes, {
 		routesDir: path.dirname(routesPath),
@@ -38,33 +41,54 @@ export default function(routesRelativePath, {
 			logger.notice("Successfully compiled client JavaScript.");
 		});
 	} else {
-		const startJsServer = hot ? startHotLoadJsServer : startStaticJsServer;
+		const startServers = (keys) => {
+			const startJsServer = hot ? startHotLoadJsServer : startStaticJsServer;
 
-		logger.notice("Starting servers...")
-		Promise.all([
-			jsUrl ? Promise.resolve() : startJsServer(compiler, jsPort),
-			startHtmlServer(serverRoutes, port),
-		])
+			logger.notice("Starting servers...")
+			Promise.all([
+				jsUrl ? Promise.resolve() : startJsServer(compiler, jsPort, keys),
+				startHtmlServer(serverRoutes, port, keys),
+			])
 			.then(
 				() => logger.notice(`Ready for requests on port ${port}.`),
 				(e) => {throw e}
 			);
+		}
+
+		if (httpsOptions) {
+			pem.createCertificate({days:1, selfSigned:true}, (err, keys) => {
+				if (err) throw err;
+				startServers({key: keys.serviceKey, cert:keys.certificate});
+			});
+		} else {
+			startServers();
+		}
 	}
 }
 
-const startHtmlServer = (serverRoutes, port) => {
-	logger.info("Starting HTML server...");
+const startHtmlServer = (serverRoutes, port, {key, cert} = {}) => {
+	return new Promise((resolve) => {
+		logger.info("Starting HTML server...");
 
-	const server = express();
-	server.use(compression());
-	triton.middleware(server, require(serverRoutes));
+		const server = express();
+		server.use(compression());
+		triton.middleware(server, require(serverRoutes));
 
-	http.createServer(server).listen(port, function () {
-		logger.info(`Started HTML server on port ${port}`);
+		if (key && cert) {
+			https.createServer({key, cert}, server).listen(port, () => {
+				logger.info(`Started HTML server over HTTPS on port ${port}`);
+				resolve();
+			});
+		} else {
+			http.createServer(server).listen(port, () => {
+				logger.info(`Started HTML server over HTTP on port ${port}`);
+				resolve();
+			});
+		}
 	});
 };
 
-const startStaticJsServer = (compiler, port) => {
+const startStaticJsServer = (compiler, port, {key, cert} = {}) => {
 	return new Promise((resolve) => {
 		compiler.run(function(err, stats) {
 			handleCompilationErrors(err, stats);
@@ -75,27 +99,35 @@ const startStaticJsServer = (compiler, port) => {
 			server.use('/', compression(), express.static('__clientTemp/build'));
 			logger.info("Starting static JavaScript server...");
 
-			http.createServer(server).listen(port, function() {
-				logger.info(`Started static JavaScript server on port ${port}`);
-				resolve();
-			});
+			if (key && cert) {
+				https.createServer({key, cert}, server).listen(port, () => {
+					logger.info(`Started static JavaScript server over HTTPS on port ${port}`);
+					resolve();
+				});
+			} else {
+				http.createServer(server).listen(port, function() {
+					logger.info(`Started static JavaScript server over HTTP on port ${port}`);
+					resolve();
+				});
+			}
 		});
 	});
 };
 
-const startHotLoadJsServer = (compiler, port) => {
+const startHotLoadJsServer = (compiler, port, httpsOptions) => {
 	logger.info("Starting hot reload JavaScript server...");
 	const compiledPromise = new Promise((resolve) => compiler.plugin("done", () => resolve()));
 	const jsServer = new WebpackDevServer(compiler, {
 		noInfo: true,
 		hot: true,
 		headers: { 'Access-Control-Allow-Origin': '*' },
+		https: httpsOptions,
 	});
 	const serverStartedPromise = new Promise((resolve) => {
 		jsServer.listen(port, () => resolve() );
 	});
 	return Promise.all([compiledPromise, serverStartedPromise])
-		.then(() => logger.info(`Started hot reload JavaScript server on port ${port}`));
+		.then(() => logger.info(`Started hot reload JavaScript server over ${httpsOptions ? "HTTPS" : "HTTP"} on port ${port}`));
 };
 
 const handleCompilationErrors = (err, stats) => {
