0721187abe5f6dc5432f95a124a90767fa69effc
Bo Borgerson <gigabo@gmail.com>
2016-04-07 16:15:18 -0700

    Add a basic test page for <RootElement when={...}>

--- packages/react-server-test-pages/routes.js
+++ packages/react-server-test-pages/routes.js
@@ -1,9 +1,13 @@
+const _ = require('lodash');
+
 module.exports = {
 	// these will be applied to every page in the site.
-	middleware: [],
+	middleware: [
+		"./middleware/RequestToPort",
+	],
 
 	// this maps URLs to modules that export a Page class.
-	routes: {
+	routes: _.assign({
 		Index: {
 			path: ["/"],
 			method: "get",
@@ -14,5 +18,11 @@ module.exports = {
 			method: "get",
 			page: "./pages/data/delay",
 		},
-	},
+	}, _.reduce(require('./entrypoints'), (obj, val, key) => {
+		if (!val.method) val.method = "get";
+		if (!val.path) val.path = [val.entry];
+		val.page = `./pages${val.entry}`;
+		obj[key] = val;
+		return obj;
+	}, {})),
 }
