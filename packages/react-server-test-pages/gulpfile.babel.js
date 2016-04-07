40d72d30aee4af6b952d3811432724658d7096df
Bo Borgerson <gigabo@gmail.com>
2016-04-07 11:50:39 -0700

    Make prepublish build lint + nsp

--- packages/react-server-test-pages/gulpfile.babel.js
+++ packages/react-server-test-pages/gulpfile.babel.js
@@ -1,9 +1,15 @@
 const eslint = require("gulp-eslint");
 const gulp = require("gulp");
+const nsp = require("gulp-nsp");
+const path = require("path");
 
 const SRC = ["**/*.js", "!node_modules/**", "!__clientTemp/**"];
 
-gulp.task("travis-ci", ["eslint"]);
+gulp.task("travis-ci", ["build"]);
+
+gulp.task("build", ["eslint", "nsp"]);
+
+gulp.task('nsp', cb => nsp({package: path.resolve('package.json')}, cb));
 
 gulp.task("eslint", [], () => gulp.src(SRC)
 	.pipe(eslint())
