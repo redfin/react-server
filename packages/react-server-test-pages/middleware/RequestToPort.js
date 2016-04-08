fdcdf9f13958250fb7ab5e85faef1dd0505f2e57
Bo Borgerson <gigabo@gmail.com>
2016-04-08 12:58:29 -0700

    Add a navigation playground to test-pages
    - Lots of data requests with ever increasing response time
    - Links between pages
        - Normal links
        - Client transition links

--- packages/react-server-test-pages/middleware/RequestToPort.js
+++ packages/react-server-test-pages/middleware/RequestToPort.js
@@ -1,10 +1,12 @@
 import {ReactServerAgent} from "react-server";
 export default class RequestToPortMiddleware {
 	handleRoute(next){
-		ReactServerAgent.plugRequest(req => {
-			// TODO: Get port dynamically?
-			req.urlPrefix('localhost:3000');
-		});
+		if (typeof window === "undefined"){
+			ReactServerAgent.plugRequest(req => {
+				// TODO: Get port dynamically?
+				req.urlPrefix('localhost:3000');
+			});
+		}
 		return next();
 	}
 }
---
+++ packages/react-server-test-pages/pages/navigation/playground.js
@@ -0,0 +1,67 @@
+/* eslint-disable no-unused-vars */
+import _ from "lodash";
+import React from "react";
+import {
+	Link,
+	ReactServerAgent,
+	RootContainer,
+} from "react-server";
+
+require('./playground.css');
+
+const ROWS = _.range(32);
+const BASE = "/navigation/playground";
+
+function GET(row) {
+	const ms  = row*16;
+	const val = JSON.stringify({row, ms});
+	return ReactServerAgent
+		.get('/data/delay', {ms, val})
+		.then(res => _.assign({}, this, res.body));
+}
+
+const RowIndex = ({row}) => <div className="row-index">Row {row}</div>;
+
+const RowMS = ({ms}) => <div className="row-ms">{ms}ms</div>;
+
+const PagePointer = ({page, row}) => <div className="page-pointer">
+	{+page === +row ? "➟" : ""}
+</div>;
+
+const NormalLink = ({row}) => <a href={`${BASE}?page=${row}`}>Normal Link</a>
+
+const ClientTransitionLink = ({row}) => <Link path={`${BASE}?page=${row}`}>Client Transition</Link>
+
+class ClientRenderIndicator extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {emoji: "⌛️"};
+	}
+	componentDidMount() {
+		this.setState({emoji: "✓"});
+	}
+	render() {
+		return <div className="render-indicator">
+			{this.state.emoji}
+		</div>
+	}
+}
+
+export default class NavigationPlaygroundPage {
+	handleRoute(next) {
+		const {page} = this.getRequest().getQuery();
+		this.data = ROWS.map(GET.bind({page}));
+
+		return next();
+	}
+	getElements() {
+		return this.data.map(promise => <RootContainer when={promise} className="row">
+			<PagePointer />
+			<RowIndex />
+			<RowMS />
+			<ClientRenderIndicator />
+			<NormalLink />
+			<ClientTransitionLink />
+		</RootContainer>);
+	}
+}
