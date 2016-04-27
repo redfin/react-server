de61fb7f4c471fd33019b7357303ca3a14f96538
Bo Borgerson <gigabo@gmail.com>
2016-04-27 16:05:13 -0700

    Gather request opts from pageload history stack frame
    This is the `frameback in => back => refresh => forward` navigation pattern.

--- packages/react-server/core/ClientController.js
+++ packages/react-server/core/ClientController.js
@@ -232,7 +232,7 @@ class ClientController extends EventEmitter {
 				// This also _replaces_ state with the request
 				// URL, which handles client-side redirects.
 				this._history.replaceState(
-					getHistoryStateFrame(),
+					getHistoryStateFrame(request),
 					null,
 					url
 				)
@@ -700,37 +700,44 @@ class ClientController extends EventEmitter {
 		var location = window.location;
 		var path = location.pathname + location.search;
 		this._initializeHistoryListener(this.context);
-		this.context.navigate(new ClientRequest(path));
+
+		// If this is a _refresh_ there may be some request options
+		// stashed in the history navigation stack frame we're sitting
+		// on.
+		const state = this._history.canClientNavigate() && history.state;
+
+		this._navigateWithHistoryState({
+			path,
+			state,
+			type: History.events.PAGELOAD,
+		});
+	}
+
+	_navigateWithHistoryState({path, state, type, check}) {
+		const opts = (state||{}).reactServerFrame;
+
+		if (check && !opts) return; // Not our frame.
+
+		this.context.navigate(new ClientRequest(path, opts||{}), type);
 	}
 
 	/**
 	 * Initializes us to listen to back button events. When the user presses the back button, the history
 	 * listener will be called and cause a navigate() event.
 	 */
-	_initializeHistoryListener(context) {
+	_initializeHistoryListener() {
 
 		// If we're running without client navigation then we don't
 		// need a 'popstate' listener.
 		if (window.__reactServerDisableClientNavigation) return;
 
 		this._historyListener = ({state}) => {
-			const opts = (state||{}).reactServerFrame;
-
-			// Not our frame.
-			if (!opts) return;
-
-			if (context) {
-				var path = this._history.getPath();
-
-				// Pass in "popstate" because this is
-				// when a user clicks the forward/back
-				// button.
-				context.navigate(
-					new ClientRequest(path, opts),
-					History.events.POPSTATE
-				);
-
-			}
+			this._navigateWithHistoryState({
+				state,
+				path  : this._history.getPath(),
+				type  : History.events.POPSTATE, // Forward/back.
+				check : true, // Only navigate if frame is ours.
+			});
 		};
 
 		this._history = new History();
