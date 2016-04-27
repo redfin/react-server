67d1353825505f25981c4aa134a6cb9054f6fab4
Bo Borgerson <gigabo@gmail.com>
2016-04-27 10:49:09 -0700

    Kill some unused code

--- packages/react-server-data-bundle-cache/src/install.js
+++ packages/react-server-data-bundle-cache/src/install.js
@@ -2,9 +2,6 @@ import wrap from "./wrap";
 import logEvents from "./log-events";
 
 let _didInstall = 0;
-let _logger;
-
-export function setLogger(logger){ _logger = logger }
 
 export default function(opts) {
 
@@ -13,5 +10,5 @@ export default function(opts) {
 
 	wrap(opts);
 
-	logEvents(_logger);
+	logEvents();
 }
