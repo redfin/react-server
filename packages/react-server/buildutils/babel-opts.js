532de4c2c6bc7d1faf1e3bca251707db82619c90
Bo Borgerson <gigabo@gmail.com>
2016-05-11 13:52:03 -0700

    Modernize babel opts
    Oh, yeah.  That.

--- packages/react-server/buildutils/babel-opts.js
+++ packages/react-server/buildutils/babel-opts.js
@@ -1,27 +1,3 @@
-
-var ecmaScriptTransforms = [
-	"es6.arrowFunctions",
-	"es6.blockScoping",
-	"es6.classes",
-	"es6.constants",
-	"es6.destructuring",
-	// "es6.forOf",
-	"es6.modules",
-	"es6.parameters",
-	"es6.properties.computed",
-	"es6.properties.shorthand",
-	"es6.spread",
-	"es6.templateLiterals",
-	// "es6.unicodeRegex", 
-	"es7.objectRestSpread",
-	// "es7.abstractReferences",
-	// "es7.comprehensions",
-	// "es7.exponentiationOperator",
-	"react",
-];
-
 module.exports = {
-	stage:1,
-	whitelist:ecmaScriptTransforms,
-	modules: "common",
+	presets: ['es2015', 'react'],
 }
