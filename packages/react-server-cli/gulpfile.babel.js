00b041e9918a08138d274a11c26863dc56d08c1c
Doug Wade <doug.wade@redfin.com>
2016-05-25 10:32:40 -0700

    Build hygenie; try to log more information about the failure

--- packages/react-server-cli/gulpfile.babel.js
+++ packages/react-server-cli/gulpfile.babel.js
@@ -2,25 +2,21 @@ import eslint from "gulp-eslint";
 import gulp from "gulp";
 import babel from "gulp-babel";
 import logging from "react-server-gulp-module-tagger";
+import gutil from "gulp-util";
 
 gulp.task("default", () => {
 	return gulp.src("src/**/*.js")
-		.pipe(logging({trim: 'react-server/packages/'}))
+		.pipe(logging({trim: "react-server/packages/"}))
 		.pipe(babel())
-		.pipe(gulp.dest("target"));
+		.pipe(gulp.dest("target"))
+		.on("error", gutil.log);
 });
 
 gulp.task("eslint", [], () => {
 	return gulp.src("src/*.js")
-        // eslint() attaches the lint output to the eslint property
-        // of the file object so it can be used by other modules.
-        .pipe(eslint())
-        // eslint.format() outputs the lint results to the console.
-        // Alternatively use eslint.formatEach() (see Docs).
-        .pipe(eslint.format())
-        // To have the process exit with an error code (1) on
-        // lint error, return the stream and pipe to failOnError last.
-        .pipe(eslint.failAfterError());
+				.pipe(eslint())
+				.pipe(eslint.format())
+				.pipe(eslint.failAfterError());
 });
 
 // there are no tests for this project :(
--- packages/react-server-data-bundle-cache/gulpfile.js
+++ packages/react-server-data-bundle-cache/gulpfile.js
@@ -1,32 +1,34 @@
-const path   = require('path');
-const gulp   = require('gulp');
-const nsp    = require('gulp-nsp');
-const babel  = require('gulp-babel');
-const eslint = require('gulp-eslint');
-const rename = require('gulp-rename');
-const tagger = require('react-server-gulp-module-tagger');
+const path   = require("path");
+const gulp   = require("gulp");
+const nsp    = require("gulp-nsp");
+const babel  = require("gulp-babel");
+const eslint = require("gulp-eslint");
+const rename = require("gulp-rename");
+const tagger = require("react-server-gulp-module-tagger");
 
 const SRC = "src/**/*.js";
 
-gulp.task('build', ['build-index', 'build-lib']);
+gulp.task("build", ["build-index", "build-lib"]);
 
-gulp.task('build-index', () => gulp.src('main.js')
-	.pipe(tagger({trim: 'react-server/packages/'}))
-	.pipe(rename('index.js'))
+gulp.task("build-index", () => gulp.src("main.js")
+	.pipe(tagger({trim: "react-server/packages/"}))
+	.pipe(rename("index.js"))
 	.pipe(gulp.dest("."))
+	.on("error", gutil.log)
 )
 
-gulp.task('build-lib', () => gulp.src(SRC)
+gulp.task("build-lib", () => gulp.src(SRC)
 	.pipe(babel())
 	.pipe(gulp.dest("./lib"))
+	.on("error", gutil.log)
 );
 
-gulp.task('eslint', [], () =>  gulp.src(SRC)
+gulp.task("eslint", [], () =>  gulp.src(SRC)
 	.pipe(eslint())
 	.pipe(eslint.format())
 	.pipe(eslint.failAfterError())
 );
 
-gulp.task('nsp', (cb) => nsp({package: path.resolve('package.json')}, cb));
+gulp.task("nsp", (cb) => nsp({package: path.resolve("package.json")}, cb));
 
-gulp.task('test', ['nsp', 'eslint']);
+gulp.task("test", ["nsp", "eslint"]);
