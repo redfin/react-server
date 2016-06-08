f7c7e0cb2b8186d75a4bb4ed959e6f42a6357b43
Doug Wade <doug.wade@redfin.com>
2016-06-08 02:19:06 -0700

    Replace configurable token with module tag

--- packages/babel-plugin-react-server/src/index.js
+++ packages/babel-plugin-react-server/src/index.js
@@ -1,6 +1,25 @@
+import loggerSpec from 'react-server-module-tagger';
+
 export default function({types: t }) {
   return {
     visitor: {
+      IdentifierExpression(node) {
+        const {name} = node;
+        console.log(`node ${node}`);
+        console.log(`name ${name}`);
+
+        // TODO: get module identifier from config
+        const fn = this.file.opts.filename
+
+        // technically, channel and cache are only reserved words for future use
+        // but let's replace them as a gentle reminder (and convenience for
+        // forward-facing users)
+        if (name === "__LOGGER__" || name === "__CHANNEL__" || name === "__CACHE__") {
+          console.log('found __LOGGER__ statment');
+        } else if (name === "") {
+          console.log(`found replacement token ${name}`);
+        }
+      }
     }
   };
 }
