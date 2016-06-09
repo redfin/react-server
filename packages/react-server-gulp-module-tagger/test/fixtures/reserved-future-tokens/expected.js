bbf5d07037c697f2e4f2c207490f0e19d686ee1f
Doug Wade <doug.wade@redfin.com>
2016-06-08 21:11:13 -0700

    Add reserved future keywords test

---
+++ packages/react-server-gulp-module-tagger/test/fixtures/reserved-future-tokens/expected.js
@@ -0,0 +1,2 @@
+{"name":"react-server-gulp-module-tagger.test.fixtures.reserved-future-tokens.actual","color":{"server":205,"client":"rgb(212,42,127)"}}
+{"name":"react-server-gulp-module-tagger.test.fixtures.reserved-future-tokens.actual","color":{"server":205,"client":"rgb(212,42,127)"}}
