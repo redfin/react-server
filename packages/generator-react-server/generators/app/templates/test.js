211ad97c1a72593fd4d196741d4cff3dc4314d93
Douglas Wade <doug@dougwade.io>
2016-06-10 12:46:51 -0700

    Run generator end-to-end test on unique port (#282)

--- packages/generator-react-server/generators/app/templates/test.js
+++ packages/generator-react-server/generators/app/templates/test.js
@@ -39,7 +39,7 @@ function getResponseCode(url) {
 	return new Promise((resolve, reject) => {
 		const req = http.get({
 			hostname: 'localhost',
-			port: 3000,
+			port: 3010,
 			path: url
 		}, res => {
 			resolve(res.statusCode);
