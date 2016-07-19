4978ecb60c93e287240207a7efe9ff39a6039007
Tim Duffey <tim.duffey@redfin.com>
2016-07-19 15:49:39 -0700

    Added an examples and features section for the homepage.

---
+++ packages/react-server-website/components/homepage/Features.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class HighlevelFeaturesList extends React.Component {
+	render() {
+		return (
+			<section>
+				<h2>What is React Server?</h2>
+				<p>It is awesome! It is the <em>only</em> server you will ever need. Don't even think about using any
+				other solution. You will be sorry.</p>
+			</section>
+		);
+	}
+}
--- packages/react-server-website/pages/homepage.js
+++ packages/react-server-website/pages/homepage.js
@@ -2,13 +2,17 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import BannerSection from '../components/homepage/HomepageBanner';
+import Example from '../components/homepage/HomepageExample';
+import Features from '../components/homepage/Features';
 
 export default class Homepage {
 	getElements() {
 		return (
-			<div className="content">
+			<div className="homepage">
 				<Header/>
 				<BannerSection/>
+				<Example/>
+				<Features/>
 				<Footer/>
 			</div>
 		);
