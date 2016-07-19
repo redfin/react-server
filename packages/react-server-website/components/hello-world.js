f43add2fccb0d188b79991aa274f7c0cfd322aee
Doug Wade <doug.wade@redfin.com>
2016-07-19 10:37:31 -0700

    Add infrastructure

---
+++ packages/react-server-website/components/hello-world.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {logging} from 'react-server';
+
+const logger = logging.getLogger(__LOGGER__);
+
+export default class HelloWorld extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {exclamationCount: 0};
+		this.handleClick = () => {
+			logger.info(`Getting more excited! previously ${this.state.exclamationCount} excitements.`);
+			this.setState({exclamationCount: this.state.exclamationCount + 1});
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<h2>Hello, World{'!'.repeat(this.state.exclamationCount)}</h2>
+				<button onClick={this.handleClick}>Get More Excited!</button>
+			</div>
+			);
+	}
+}
