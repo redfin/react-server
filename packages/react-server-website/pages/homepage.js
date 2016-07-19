3627ebb8a1fb0ffbd249b67af31976a6119fdfc3
Tim Duffey <tim.duffey@redfin.com>
2016-07-19 15:27:40 -0700

    Added different page components including header and footer.

--- packages/react-server-website/pages/homepage.js
+++ packages/react-server-website/pages/homepage.js
@@ -2,38 +2,16 @@ import React from 'react';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import BannerSection from '../components/homepage/HomepageBanner';
-import HelloWorld from '../components/hello-world';
-import {ReactServerAgent} from "react-server";
-import Example from '../components/homepage/HomepageExample';
-import Features from '../components/homepage/Features';
-
-// Syntax highlighting - runs on the fly.
-// TODO move to middleware
-require('prismjs');
-require('./prism.css');
-
-const getData = path => ReactServerAgent
-	.get("/api/docs", {path})
-	.then(res => res.body);
 
 export default class Homepage {
 	getElements() {
-		const path = "/docs/client-transitions.md";
-
-		return [
-			getData(path).then(function (res) {
-				return (
-					<div className="content">
-						<Header/>
-						<HelloWorld {...res} />
-						<BannerSection/>
-						<Example/>
-						<Features/>
-						<Footer/>
-					</div>
-				);
-			}),
-		];
+		return (
+			<div className="content">
+				<Header/>
+				<BannerSection/>
+				<Footer/>
+			</div>
+		);
 	}
 
 	getMetaTags() {
