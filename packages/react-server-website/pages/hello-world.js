d668bead98ce7ad9ef774d05c7f6d41f9b341e52
Bo Borgerson <gigabo@gmail.com>
2016-07-19 11:04:47 -0700

    Slim down deps
    - Use eslint
    - Ditch ava

--- packages/react-server-website/pages/hello-world.js
+++ packages/react-server-website/pages/hello-world.js
@@ -12,7 +12,7 @@ export default class SimplePage {
 			{'http-equiv': 'x-ua-compatible', 'content': 'ie=edge'},
 			{name: 'viewport', content: 'width=device-width, initial-scale=1'},
 			{name: 'description', content: 'hello world, powered by React Server'},
-			{name: 'generator', content: 'React Server'}
+			{name: 'generator', content: 'React Server'},
 		];
 	}
 }
