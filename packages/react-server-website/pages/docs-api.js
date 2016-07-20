7a76fad4a55f65e09e06675d47c38f6fb4cfe544
Bo Borgerson <gigabo@gmail.com>
2016-07-20 11:18:54 -0700

    Munge links within docs (#370)
    Example: `/client-transitions.md` => `/docs/client-transitions`

--- packages/react-server-website/pages/docs-api.js
+++ packages/react-server-website/pages/docs-api.js
@@ -15,6 +15,26 @@ export default class DocsApi {
 		const {path} = this.getRequest().getQuery();
 		return ReactServerAgent
 			.get(URL(path))
-			.then(({text}) => JSON.stringify({path, text}));
+			.then(({text}) => text)
+			.then(transformLinks)
+			.then(text => JSON.stringify({path, text}));
 	}
+}
+
+function transformLink(match, text, path) {
+	if (path.indexOf("http") !== 0) {
+		if (path.indexOf("/docs") !== 0) {
+			if (path.indexOf("/") === 0) {
+				path = "/docs" + path;
+			} else {
+				path = "/docs/" + path;
+			}
+		}
+		path = path.replace(/\.md$/, "");
+	}
+	return `[${text}](${path})`;
+}
+
+function transformLinks(text) {
+	return text.replace(/\[([^\]]+)\]\(([^)]+)\)/g, transformLink);
 }
