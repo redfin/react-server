8cb96721006fd7f218506df11e968b74ef489c87
David Alber <david.alber@redfin.com>
2016-07-21 13:59:20 -0700

    Adding Ansible playbook to provision machine and set up docker

---
+++ packages/react-server-website/provisioning/docker-setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list'
+sudo apt-get update
+sudo apt-get install -y linux-image-extra-$(uname -r)
+sudo apt-get install -y apparmor
+sudo apt-get install -y docker-engine
