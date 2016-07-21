5682837b41918ca42099534e8fc786c4bd58e2cd
Bo Borgerson <gigabo@gmail.com>
2016-07-20 17:25:39 -0700

    Get table of contents onto docs pages
    Links don't work yet... need to figure out routes

--- packages/react-server-website/components/doc-body.js
+++ packages/react-server-website/components/doc-body.js
@@ -1,3 +1,4 @@
+import React from "react";
 import {Component} from 'react';
 import Markdown from './Markdown';
 
--- packages/react-server-website/lib/get-doc-body.js
+++
@@ -1,7 +0,0 @@
-import {ReactServerAgent} from "react-server";
-
-export default function getDocBody(path) {
-	return ReactServerAgent
-		.get("/api/docs", {path})
-		.then(res => res.body);
-}
