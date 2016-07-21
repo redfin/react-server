55cfb968abd9f4d540474c44f00946b1274601b6
Eric Gideon <eric.gideon@redfin.com>
2016-07-21 10:43:50 -0700

    initial doc-contents layout; revisions to page layout; changes to homepage styles

--- packages/react-server-website/pages/homepage.js
+++ packages/react-server-website/pages/homepage.js
@@ -6,7 +6,7 @@ import './homepage.less';
 export default class Homepage {
 	getElements() {
 		return (
-			<div className="homepage">
+			<div className="homepage rootContent">
 				{/* TODO: Change from hardcoded serverVersion to dynamic. */}
 				<Content serverVersion="0.3.4"/>
 				<Footer/>
@@ -14,13 +14,13 @@ export default class Homepage {
 		);
 	}
 
-	getMetaTags() {
-		return [
-			{charset: 'utf8'},
-			{'http-equiv': 'x-ua-compatible', 'content': 'ie=edge'},
-			{name: 'viewport', content: 'width=device-width, initial-scale=1'},
-			{name: 'description', content: 'hello world, powered by React Server'},
-			{name: 'generator', content: 'React Server'},
-		];
-	}
+	// getMetaTags() {
+	// 	return [
+	// 		{charset: 'utf8'},
+	// 		{'http-equiv': 'x-ua-compatible', 'content': 'ie=edge'},
+	// 		{name: 'viewport', content: 'width=device-width, initial-scale=1'},
+	// 		{name: 'description', content: 'hello world, powered by React Server'},
+	// 		{name: 'generator', content: 'React Server'},
+	// 	];
+	// }
 }
