2e3caacdf578bd18e9d92be0b9121eac70d55070
Bo Borgerson <gigabo@gmail.com>
2016-07-22 10:04:44 -0700

    Expose the page title
    Via a static method on the contents component.
    
    ```js
    DocContents.activePageName()
    ```

--- packages/react-server-website/pages/docs.js
+++ packages/react-server-website/pages/docs.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import {join} from "path";
-import {
-	getCurrentRequestContext,
-	RootContainer,
-	RootElement,
-} from "react-server";
+import {RootContainer, RootElement} from "react-server";
 
 import Repo from "../lib/repo";
 import DocBody from "../components/doc-body";
@@ -14,18 +10,12 @@ export default class DocsPage {
 	handleRoute(next) {
 		const path = this.getRequest().getRouteParams().path || "README";
 		this.bodyPromise = Repo.getFile(join("/docs", `${path}.md`));
-		this.contentsPromise = Repo.getContents();
+		this.contentsPromise = Repo.getContents().then(DocContents.setResponse);
 		return next();
 	}
 
 	getTitle() {
-		return this.contentsPromise.then(res => {
-			const path = getCurrentRequestContext().getCurrentPath()
-				.replace("/docs/", "");
-			return (res.contents.reduce((page, section) => (
-				page || section.pages.find(page => page.path === path)
-			), null) || {name: "React Server Documentation"}).name;
-		});
+		return this.contentsPromise.then(() => DocContents.activePageName());
 	}
 
 	getElements() {
