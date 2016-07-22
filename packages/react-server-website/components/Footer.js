a367f7a470679bf334a2108c09813cbfc68e46a4
Eric Gideon <eric.gideon@redfin.com>
2016-07-22 12:20:47 -0700

    add buttons to footer, fix keys in header

--- packages/react-server-website/components/Footer.js
+++ packages/react-server-website/components/Footer.js
@@ -15,6 +15,10 @@ export default class Footer extends React.Component {
 						<a href="http://www.apache.org/licenses/LICENSE-2.0">Apache License 2.0</a> Talk with us on <a href="https://react-server.slack.com/">Slack</a>.
 					</small>
 				</p>
+				<a href="https://github.com/redfin/react-server" className="github-button" data-count-href="/redfin/react-server/stargazers" data-count-api="/repos/redfin/react-server#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star redfin/react-server on GitHub">Star</a>
+				&nbsp;
+				<script async defer src="http://slack.react-server.io/slackin.js"></script>
+				<script async defer src="https://buttons.github.io/buttons.js"></script>
 			</footer>
 		);
 	}
