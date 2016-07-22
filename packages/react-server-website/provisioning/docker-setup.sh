3b4020e96046fffdbfcae391984fee4feda73c72
David Alber <david.alber@redfin.com>
2016-07-21 17:37:51 -0700

    Installing Docker Compose on new instances

--- packages/react-server-website/provisioning/docker-setup.sh
+++ packages/react-server-website/provisioning/docker-setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Get the system ready.
 sudo apt-get update
 sudo apt-get install -y apt-transport-https ca-certificates
 sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
@@ -7,4 +8,10 @@ sudo sh -c 'echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /
 sudo apt-get update
 sudo apt-get install -y linux-image-extra-$(uname -r)
 sudo apt-get install -y apparmor
+
+# Install Docker Engine.
 sudo apt-get install -y docker-engine
+
+# Set up Docker Compose.
+sudo sh -c "curl -L https://github.com/docker/compose/releases/download/1.7.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose"
+sudo chmod +x /usr/local/bin/docker-compose
