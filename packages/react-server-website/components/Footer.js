90dd6d1e2f23ca0b25f027b967df97f8d4899609
Bo Borgerson <gigabo@gmail.com>
2016-07-29 15:36:19 -0700

    Re-wire github/slackin badges after client transition (#467)
    Losing them after the first click right now.

--- packages/react-server-website/components/Footer.js
+++ packages/react-server-website/components/Footer.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import wireUpGitHubButton from '../lib/github-button';
+import wireUpSlackinButton from '../lib/slackin-button';
 import './Footer.less';
 
 export default class Footer extends React.Component {
@@ -18,8 +20,12 @@ export default class Footer extends React.Component {
 				<a href="https://github.com/redfin/react-server" className="github-button" data-count-href="/redfin/react-server/stargazers" data-count-api="/repos/redfin/react-server#stargazers_count" data-count-aria-label="# stargazers on GitHub" aria-label="Star redfin/react-server on GitHub">Star</a>
 				&nbsp;
 				<script async defer src="https://slack.react-server.io/slackin.js"></script>
-				<script async defer src="https://buttons.github.io/buttons.js"></script>
 			</footer>
 		);
+	}
+
+	componentDidMount() {
+		wireUpGitHubButton();
+		wireUpSlackinButton();
 	}
 }
