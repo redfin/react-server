99b7227cab89db1c9f5ea1ec6202e82253314b10
Kevin Jonson <kcjonson@gmail.com>
2016-08-13 09:56:15 -0700

    Revert "updates to attributes test page"
    This reverts commit 9a6d065c9dba5e44c9d5a0c3f018433169b3efc9.

--- packages/react-server-test-pages/pages/root/attributes.js
+++ packages/react-server-test-pages/pages/root/attributes.js
@@ -1,9 +1,8 @@
 import {RootContainer, RootElement, Link} from "react-server";
-require("./attributes.css");
 
 const url = color => `/root/attributes?color=${color}`;
 
-const ColorLinks = opts => <span>
+const Links = opts => <span>
 	<Link path={url( "red"    )} {...opts}>Red</    Link><span> | </span>
 	<Link path={url( "yellow" )} {...opts}>Yellow</ Link><span> | </span>
 	<Link path={url( "green"  )} {...opts}>Green</  Link>
@@ -14,33 +13,16 @@ export default class RootAttributesPage {
 		const color = this.getRequest().getQuery().color || "white";
 		const style = `background-color: ${color}`;
 		return <RootContainer>
-
-			<h2>style</h2>
-			<div><ColorLinks /> (normal)</div>
-			<div><ColorLinks reuseDom={true}/> (reuseDom)</div>
+			<div>Background below the hr should be <em>{color}</em></div>
+			<div><Links /> (normal)</div>
+			<div><Links reuseDom={true}/> (reuseDom)</div>
+			<hr />
 			<RootContainer style={style}>
-				<div className="example">RootContainer should be {color}</div>
+				<div>RootContainer</div>
 			</RootContainer>
 			<RootElement style={style}>
-				<div className="example">RootElement should be {color}</div>
+				<div>RootElement</div>
 			</RootElement>
-
-			<h2>className</h2>
-			<RootContainer className="example-class">
-				<div className="example">RootContainer should be "salmon"</div>
-			</RootContainer>
-			<RootElement className="example-class">
-				<div className="example">RootElement should be "salmon"</div>
-			</RootElement>
-
-			<h2>tagName</h2>
-			<RootContainer tagName="section">
-				<div className="example">RootContainer should be "turquoise"</div>
-			</RootContainer>
-			<RootElement tagName="section">
-				<div className="example">RootElement should be "turquoise"</div>
-			</RootElement>
-
 		</RootContainer>
 	}
 }
