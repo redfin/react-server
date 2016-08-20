0e85aa1bd9e9c6055cdfec58d51de5addc0d4d09
Harley Bussell <modmac@gmail.com>
2016-08-20 01:56:24 +0000

    fix indent

--- packages/react-server-cli/src/compileClient.js
+++ packages/react-server-cli/src/compileClient.js
@@ -38,11 +38,11 @@ export default (opts = {}) => {
 		throw new Error("Hot reload cannot be used with long-term caching. Please disable either long-term caching or hot reload.");
 	}
 
-  const webpackConfigFunc = (data) => { return data }
+	const webpackConfigFunc = (data) => { return data }
 	if (opts['webpack-config']) {
-    const webpackDirAbsolute = path.resolve(process.cwd(), opts['webpack-config']);
-    const userWebpackConfigFunc = require(webpackDirAbsolute)
-    webpackConfigFunc = userWebpackConfigFunc.default
+    	const webpackDirAbsolute = path.resolve(process.cwd(), opts['webpack-config']);
+    	const userWebpackConfigFunc = require(webpackDirAbsolute)
+    	webpackConfigFunc = userWebpackConfigFunc.default
 	}
 
 	const workingDirAbsolute = path.resolve(process.cwd(), workingDir);
