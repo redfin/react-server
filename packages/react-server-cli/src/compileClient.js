955f01597397625a7720494d7cedd8d72c1e9d5f
Harley Bussell <modmac@gmail.com>
2016-08-20 01:47:15 +0000

    fix indent

--- packages/react-server-cli/src/compileClient.js
+++ packages/react-server-cli/src/compileClient.js
@@ -8,6 +8,7 @@ import crypto from "crypto"
 import StatsPlugin from "webpack-stats-plugin"
 import callerDependency from "./callerDependency"
 
+
 // commented out to please eslint, but re-add if logging is needed in this file.
 // import {logging} from "react-server"
 // const logger = logging.getLogger(__LOGGER__);
@@ -37,7 +38,7 @@ export default (opts = {}) => {
 		throw new Error("Hot reload cannot be used with long-term caching. Please disable either long-term caching or hot reload.");
 	}
 
-    var webpackConfigFunc = (data) => { return data }
+  var webpackConfigFunc = (data) => { return data }
 	if (opts['webpack-config']) {
 		const webpackDirAbsolute = path.resolve(process.cwd(), opts['webpack-config']);
 		var userWebpackConfigFunc = require(webpackDirAbsolute)
