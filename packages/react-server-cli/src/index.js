67a4eede4fb68ef4536ec59f877bd138b03a856e
JB <jasonbenesch@gmail.com>
2016-08-26 11:56:57 -0700

    trigger happy and missed the import

--- packages/react-server-cli/src/index.js
+++ packages/react-server-cli/src/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const run = require("./run").default;
 const parseCliArgs = require("./parseCliArgs").default;
+const defaultOptions = require("./defaultOptions").default;
 
 require.extensions['.css'] =
 require.extensions['.less'] =
