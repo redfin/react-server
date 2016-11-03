0d65211b83859ead042c9b075e91ade433ed5de9
Bo Borgerson <gigabo@gmail.com>
2016-11-03 16:18:56 -0700

    Run eslint on the babel plugin test harness

--- packages/babel-plugin-react-server/test/index.js
+++ packages/babel-plugin-react-server/test/index.js
@@ -2,25 +2,24 @@ import path from 'path';
 import fs from 'fs';
 import assert from 'assert';
 import { transformFileSync } from 'babel-core';
-import plugin from '../src';
 
 function trim(str) {
-  return str.replace(/^\s+|\s+$/, '');
+	return str.replace(/^\s+|\s+$/, '');
 }
 
 describe('React Server transpilation', () => {
-  const fixturesDir = path.join(__dirname, 'fixtures');
-  fs.readdirSync(fixturesDir).map((caseName) => {
-    it(`should ${caseName.split('-').join(' ')}`, () => {
-      const fixtureDir = path.join(fixturesDir, caseName);
-      const actualPath = path.join(fixtureDir, 'actual.js');
-      const actual = transformFileSync(actualPath).code;
+	const fixturesDir = path.join(__dirname, 'fixtures');
+	fs.readdirSync(fixturesDir).map((caseName) => {
+		it(`should ${caseName.split('-').join(' ')}`, () => {
+			const fixtureDir = path.join(fixturesDir, caseName);
+			const actualPath = path.join(fixtureDir, 'actual.js');
+			const actual = transformFileSync(actualPath).code;
 
-      const expected = fs.readFileSync(
-          path.join(fixtureDir, 'expected.js')
-      ).toString();
+			const expected = fs.readFileSync(
+				path.join(fixtureDir, 'expected.js')
+			).toString();
 
-      assert.equal(trim(actual), trim(expected));
-    });
-  });
+			assert.equal(trim(actual), trim(expected));
+		});
+	});
 });
