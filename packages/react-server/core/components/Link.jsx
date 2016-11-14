d1c6a1a41aa2d2f7098329fefdeac500c3f6a17d
devoiddesign <user@users-Mac-235.local>
2016-11-14 12:06:35 -0800

    Added IE8 & IE9 detection

--- packages/react-server/core/components/Link.jsx
+++ packages/react-server/core/components/Link.jsx
@@ -1,52 +1,1 @@
-
-var React = require('react'),
-	navigateTo = require("../util/navigateTo");
-
-module.exports = React.createClass({
-	displayName: 'Link',
-
-	propTypes: {
-		path       : React.PropTypes.string,
-		href       : React.PropTypes.string,
-		bundleData : React.PropTypes.bool,
-		frameback  : React.PropTypes.bool,
-		reuseDom   : React.PropTypes.bool,
-		className  : React.PropTypes.string,
-	},
-
-	getDefaultProps(){
-		return {
-			bundleData : false,
-			frameback  : false,
-			reuseDom   : false,
-			reuseFrame : false,
-		}
-	},
-
-	render: function () {
-		return (
-			<a href={this.props.path || this.props.href} onClick={this._onClick} className={this.props.className}>{this.props.children}</a>
-		);
-	},
-
-	_onClick: function (e) {
-
-		// TODO: IE8-9 detection
-
-		// TODO: if OSX && key.isMeta?
-		if (!e.metaKey) {
-			e.preventDefault();
-			e.stopPropagation();
-			const {bundleData, frameback, reuseDom, reuseFrame} = this.props;
-			navigateTo(this.props.path || this.props.href, {
-				bundleData,
-				frameback,
-				reuseDom,
-				reuseFrame,
-			});
-		} else {
-			// do normal browser navigate
-		}
-
-	},
-})
+﻿var React = require('react'),	navigateTo = require("../util/navigateTo");module.exports = React.createClass({	displayName: 'Link',	propTypes: {		path       : React.PropTypes.string,		href       : React.PropTypes.string,		bundleData : React.PropTypes.bool,		frameback  : React.PropTypes.bool,		reuseDom   : React.PropTypes.bool,		className  : React.PropTypes.string,	},	getDefaultProps(){		return {			bundleData : false,			frameback  : false,			reuseDom   : false,			reuseFrame : false,		}	},	render: function () {		return (			<a href={this.props.path || this.props.href} onClick={this._onClick} className={this.props.className}>{this.props.children}</a>		);	},	_onClick: function (e) {		// TODO: IE8-9 detection         var userAgent = navigator.userAgent,         ieTest8 = /MSIE\s8\../,         ieTest9 = /MSIE\s9\../;         userAgent = userAgent.split(";");                if (userAgent[1].match(ieTest8) || userAgent[1].match(ieText9)) {                //do something         }         		// TODO: if OSX && key.isMeta?		if (!e.metaKey) {			e.preventDefault();			e.stopPropagation();			const {bundleData, frameback, reuseDom, reuseFrame} = this.props;			navigateTo(this.props.path || this.props.href, {				bundleData,				frameback,				reuseDom,				reuseFrame,			});		} else {			// do normal browser navigate		}	},})
