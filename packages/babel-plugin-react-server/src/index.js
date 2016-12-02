2e28edcadbd58e71a2a4c2a611cf6e0ff9a06eae
Douglas Wade <douglas.b.wade@gmail.com>
2016-12-02 02:23:42 -0800

    Support labels in babel-plugin-react-server

--- packages/babel-plugin-react-server/src/index.js
+++ packages/babel-plugin-react-server/src/index.js
@@ -8,24 +8,43 @@ module.exports = function() {
 			Identifier(p, state) {
 				const {node} = p;
 				const {name} = node;
-
-				const trim = state.opts.trim;
-				const prefix = state.opts.prefix;
-				const parent = path.resolve(path.join(process.cwd(), '..')) + path.sep;
-				const filePath = this.file.opts.filename.replace(parent, '');
-
-				//TODO: Support labels
-				const moduleTag = loggerSpec({ filePath, trim, prefix });
-
-				let tokens;
-				if (state.opts.tokens) {
-					tokens = new Set(state.opts.tokens);
-				} else {
-					tokens = new Set(["__LOGGER__", "__CHANNEL__", "__CACHE__"]);
-				}
+				const tokens = getTokens(state);
 
 				if (tokens.has(name)) {
-					p.replaceWith(convertObjectToObjectExpression(JSON.parse(moduleTag)));
+					const trim = state.opts.trim;
+					const prefix = state.opts.prefix;
+					const parent = path.resolve(path.join(process.cwd(), '..')) + path.sep;
+					const filePath = this.file.opts.filename.replace(parent, '');
+					const options = JSON.parse(loggerSpec({ filePath, trim, prefix }));
+
+					p.replaceWith(convertObjectToObjectExpression(options));
+				}
+			},
+			CallExpression(p, state) {
+				const {node} = p;
+				const {callee} = node;
+				const {name} = callee;
+				const tokens = getTokens(state);
+
+				if (tokens.has(name)) {
+					const trim = state.opts.trim;
+					const prefix = state.opts.prefix;
+					const parent = path.resolve(path.join(process.cwd(), '..')) + path.sep;
+					const filePath = this.file.opts.filename.replace(parent, '');
+					const options = JSON.parse(loggerSpec({ filePath, trim, prefix }));
+
+					// We're assuming that the options string will only ever contain a
+					// single option, `label`, and that users won't call a tag as a
+					// function without providing a label.  It's the only key we support
+					// (at least, its the only one documented in the gulp module tagger),
+					// but if we ever support more, we'll need to pass the arguments as a
+					// string to loggerSpec (probably by using babel-generator to generate
+					// the code again from the ast) or change the module tagger to take an
+					// options object, rather than a string.
+					const arg = node.arguments[0];
+					options.label = arg.properties[0].value.value;
+
+					p.replaceWith(convertObjectToObjectExpression(options));
 				}
 			},
 		},
@@ -52,4 +71,12 @@ function convertObjectToObjectExpression(obj) {
 		properties.push(t.objectProperty(t.identifier(key), literal));
 	});
 	return t.objectExpression(properties);
+}
+
+function getTokens(state) {
+	if (state.opts.tokens) {
+		return new Set(state.opts.tokens);
+	} else {
+		return new Set(["__LOGGER__", "__CHANNEL__", "__CACHE__"]);
+	}
 }
---
+++ packages/babel-plugin-react-server/test/fixtures/label/expected.js
@@ -0,0 +1,8 @@
+var logger = require('react-server').logging.getLogger({
+  name: "babel-plugin-react-server.test.fixtures.label.actual",
+  color: {
+    server: 157,
+    client: "rgb(127,212,127)"
+  },
+  label: "foo"
+});
