30674d34a75c9d4e56edefd32c911fa648c8bd5a
Karl Horky <karl.horky@gmail.com>
2017-01-06 17:37:12 +0100

    Fix linting errors

--- packages/react-server-cli/src/cli.js
+++ packages/react-server-cli/src/cli.js
@@ -3,7 +3,7 @@ const path = require("path");
 const cli = require(".");
 
 cli.parseCliArgs().then(args => {
-  const config = args.configPath ? JSON.parse(fs.readFileSync(path.join(args.configPath, ".babelrc"))) : {};
-  require("babel-core/register")(config);
-  cli.run(args);
+	const config = args.configPath ? JSON.parse(fs.readFileSync(path.join(args.configPath, ".babelrc"))) : {};
+	require("babel-core/register")(config);
+	cli.run(args);
 }).catch(console.error);
