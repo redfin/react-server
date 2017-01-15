4cae91b4d85baff767815f17453649c4be172408
PC Drew <pc@thedrews.net>
2017-01-15 12:01:28 -0700

    Added tests for requests over http and https servers.

---
+++ packages/react-server-integration-tests/src/__tests__/httpAndHttpsServers/HttpServerSpec.js
@@ -0,0 +1,34 @@
+import fs from 'fs';
+import path from 'path';
+import helper from '../../specRuntime/testHelper';
+
+describe('A basic page', () => {
+	const httpsOptions = {
+		key: fs.readFileSync(path.join(__dirname, 'key.pem')),
+		cert: fs.readFileSync(path.join(__dirname, 'cert.pem')),
+	};
+
+	describe('served using http', () => {
+		helper.startServerBeforeAll(__filename, [
+			'./HelloWorldPage',
+		]);
+
+		helper.stopServerAfterAll();
+
+		helper.testWithDocument("/helloWorld", (document) =>{
+			expect(document.querySelector("div#foo").innerHTML).toMatch("Hello, world!");
+		});
+	});
+
+	describe('served using https', () => {
+		helper.startServerBeforeAll(__filename, [
+			'./HelloWorldPage',
+		], httpsOptions);
+
+		helper.stopServerAfterAll();
+
+		helper.testWithDocument("/helloWorld", (document) =>{
+			expect(document.querySelector("div#foo").innerHTML).toMatch("Hello, world!");
+		});
+	});
+});
