5cc9b71fbb1b3f2959e7f8acef6dc38c6024aa13
PC Drew <pc@thedrews.net>
2017-01-15 08:32:34 -0700

    Convert comment to TODO

--- packages/react-server-test-pages/pages/navigation/forward.js
+++ packages/react-server-test-pages/pages/navigation/forward.js
@@ -16,7 +16,8 @@ export default class ForwardPage {
 		return this.data.then((res) => {
 			//then depending on said data, forward to one of two pages, and pass along the data we pre-fetched
 			const pageName = (res.body % 2 === 0) ? "./forwardEven" : "./forwardOdd";
-            if (typeof window !== 'undefined') { //would be nice if this is `process.env.isServer`
+			// TODO: change this to use isBrowser when that check is available.
+            if (typeof window !== 'undefined') {
             	return require.ensure([pageName], () => {
 	            	return {
 	            		page: require(pageName).default,
