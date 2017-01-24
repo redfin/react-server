d299594361553b08d8550889920a2561587617e1
Sresan Thevarajah <sresan.thevarajah@redfin.com>
2017-01-23 21:59:36 -0800

    Fix errors from rebaseing

--- packages/react-server-examples/redux-basic/pages/counter-app/index.js
+++ packages/react-server-examples/redux-basic/pages/counter-app/index.js
@@ -5,34 +5,34 @@ import Counter from '../../components/Counter'
 import store from '../store'
 
 export default class CounterPage {
-  getElements() {
-    return [
-      <RootProvider store={store}>
-        <RootElement key={0}>
-          <Counter
-            value={store.getState()}
-            onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-            onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-          />
-        </RootElement>
-        <RootElement key={1}>
-          <Counter
-            value={store.getState()}
-            onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
-            onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
-          />
-        </RootElement>
-      </RootProvider>,
-    ]
-  }
+	getElements() {
+		return [
+			<RootProvider store={store}>
+				<RootElement key={0}>
+					<Counter
+						value={store.getState()}
+						onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+						onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+					/>
+				</RootElement>
+				<RootElement key={1}>
+					<Counter
+						value={store.getState()}
+						onIncrement={() => store.dispatch({ type: 'INCREMENT' })}
+						onDecrement={() => store.dispatch({ type: 'DECREMENT' })}
+					/>
+				</RootElement>
+			</RootProvider>,
+		]
+	}
 
-  getMetaTags() {
-    return [
-      {charset: 'utf8'},
-      {'http-equiv': 'x-ua-compatible', 'content': 'ie=edge'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
-      {name: 'description', content: 'Redux Counter app, powered by React Server'},
-      {name: 'generator', content: 'React Server'},
-    ]
-  }
+	getMetaTags() {
+		return [
+			{charset: 'utf8'},
+			{'http-equiv': 'x-ua-compatible', 'content': 'ie=edge'},
+			{name: 'viewport', content: 'width=device-width, initial-scale=1'},
+			{name: 'description', content: 'Redux Counter app, powered by React Server'},
+			{name: 'generator', content: 'React Server'},
+		]
+	}
 }
