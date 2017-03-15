d2d52d8b542a9b0efe25e19527f58aa29297ef6d
Bo Borgerson <gigabo@gmail.com>
2017-03-15 16:22:13 -0700

    Fix a duplicate accessor key
    Thanks @sresant!

--- packages/react-server/core/util/PageUtil.js
+++ packages/react-server/core/util/PageUtil.js
@@ -191,8 +191,8 @@ var PAGE_CHAIN_PROTOTYPE = {
 	setHasDocument     : makeSetter('hasDocument'),
 	getJsBelowTheFold  : makeGetter('jsBelowTheFold'),
 	setJsBelowTheFold  : makeSetter('jsBelowTheFold'),
-	getSplitJsLoad     : makeGetter('jsBelowTheFold'),
-	setSplitJsLoad     : makeSetter('jsBelowTheFold'),
+	getSplitJsLoad     : makeGetter('splitJsLoad'),
+	setSplitJsLoad     : makeSetter('splitJsLoad'),
 };
 
 // We log all method calls on the page chain for debugging purposes.
