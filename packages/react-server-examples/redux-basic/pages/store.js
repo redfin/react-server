bf241ffd0d1d0489eaf40af12a602b38e11d5cc6
Tomas Roos <ptomasroos@gmail.com>
2017-03-17 19:02:25 +0100

    Make sure Redux Devtools is finding the store (#894)
    Makes it easier for people evaluating with existing tools & behaviours to find redux data

--- packages/react-server-examples/redux-basic/pages/store.js
+++ packages/react-server-examples/redux-basic/pages/store.js
@@ -1,4 +1,7 @@
 import { createStore } from 'redux'
 import reducer from './counter-app/reducer'
 
-export default createStore(reducer)
+export default createStore(
+  reducer,
+  typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
