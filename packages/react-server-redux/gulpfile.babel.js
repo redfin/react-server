dffb566bd85f32bfa7c74ae9e848ff1d8c0bb51e
Sresan Thevarajah <sresan.thevarajah@redfin.com>
2017-03-27 17:11:44 -0700

    Transpile react-server-redux code

---
+++ packages/react-server-redux/gulpfile.babel.js
@@ -0,0 +1,23 @@
+const gulp = require("gulp");
+const babel = require("gulp-babel");
+const eslint = require("gulp-eslint");
+
+const SRC = ["src/*.js", "!node_modules/**"];
+
+const DEST = ".";
+
+gulp.task("eslint", [], () => gulp.src(SRC)
+	.pipe(eslint())
+	.pipe(eslint.format())
+	.pipe(eslint.failAfterError())
+);
+
+gulp.task("compile", () => gulp.src(SRC)
+	.pipe(babel())
+	.pipe(gulp.dest(DEST))
+);
+
+
+gulp.task("build", ["eslint", "compile"]);
+
+gulp.task("travis-ci", ["build"]);
--- packages/react-server-redux/index.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	ReduxAdapter: require('./ReduxAdapter'),
-	RootProvider: require('./RootProvider'),
-}
