6f79197086b31bc31ad631c34173ee8ec66f2e90
Matt Olson <matt.olson@redfin.com>
2017-06-15 15:24:35 -0700

    Implement superagent's req.withCredentials() method

--- packages/react-server/core/ReactServerAgent/__tests__/ReactServerAgentSpec.js
+++ packages/react-server/core/ReactServerAgent/__tests__/ReactServerAgentSpec.js
@@ -164,6 +164,22 @@ describe("ReactServerAgent", () => {
 				.done();
 		}));
 
+		it("supports withCredentials()", withRlsContext((done) => {
+			ReactServerAgent.get('/describe')
+				.withCredentials()
+				.then(res => {
+					expect(res).toBeDefined();
+					expect(res.status).toEqual(200);
+					done();
+				})
+				.catch (err => {
+					// this should never get called
+					expect(err).toBeUndefined();
+					done(err);
+				})
+				.done();
+		}));
+
 	});
 
 
