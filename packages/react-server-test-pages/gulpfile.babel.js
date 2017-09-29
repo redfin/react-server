40501a7b0bb9c9f7cda66e33ab03212571d1d109
Bo Borgerson <gigabo@gmail.com>
2017-09-29 15:06:23 -0700

    Move nsp from prepublish to test for test-pages

--- packages/react-server-test-pages/gulpfile.babel.js
+++ packages/react-server-test-pages/gulpfile.babel.js
@@ -7,7 +7,7 @@ const SRC = ["**/*.js", "!node_modules/**", "!__clientTemp/**"];
 
 gulp.task("travis-ci", ["build"]);
 
-gulp.task("build", ["eslint", "nsp"]);
+gulp.task("build", ["eslint"]);
 
 gulp.task('nsp', cb => nsp({package: path.resolve('package.json')}, cb));
 
@@ -18,5 +18,5 @@ gulp.task("eslint", [], () => gulp.src(SRC)
 );
 
 // There are no tests for this project, currently.
-// Just make sure it lints.
-gulp.task("test", ["eslint"]);
+// Just make sure it lints and passes nsp.
+gulp.task("test", ["eslint", "nsp"]);
