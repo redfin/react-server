3ffb291d5ffbe677bf5c091ec290e01fd0ecb558
PC Drew <pc@thedrews.net>
2019-05-21 14:44:58 -0600

    Update the CSS test to work with the new filename chunks on client transition.

--- packages/react-server-integration-tests/src/__tests__/css/CssSpec.js
+++ packages/react-server-integration-tests/src/__tests__/css/CssSpec.js
@@ -10,7 +10,9 @@ describe("A CSS page", () => {
 
 	describe("can apply a css class", () => {
 		helper.testWithDocument("/css", (document) => {
-			expect(document.querySelector("head link[rel=stylesheet]").href).toMatch(/route0\.css/);
+			// The way Webpack is chunking the CSS files, the client transition might look for 0.css while
+			// the server side looks for route0.css
+			expect(document.querySelector("head link[rel=stylesheet]").href).toMatch(/(?:route)?0\.css/);
 		});
 	});
 
@@ -26,9 +28,11 @@ describe("A CSS page with assets", () => {
 
 	describe("can apply a css class", () => {
 		helper.testWithDocument("/cssWithAssets", (document) => {
-      // this test is really just to make sure that react-server-cli doesn't explode
-      // when compiling CSS that includes images and fonts.
-			expect(document.querySelector("head link[rel=stylesheet]").href).toMatch(/route0\.css/);
+			// this test is really just to make sure that react-server-cli doesn't explode
+			// when compiling CSS that includes images and fonts.
+			// The way Webpack is chunking the CSS files, the client transition might look for 0.css while
+			// the server side looks for route0.css
+			expect(document.querySelector("head link[rel=stylesheet]").href).toMatch(/(?:route)?0\.css/);
 		});
 	});
 
